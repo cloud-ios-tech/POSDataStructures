27ebd73fb282c520d908c5f5d03af25f614f33b5
Dmitry Golubev <dmitry.golubev@vk.team>
2023-10-11 18:31:31 +0300

    добавил схемы

--- POSDataStructures/POSDataStructures.h
+++ POSDataStructures/POSDataStructures.h
@@ -2,8 +2,20 @@
 //  POSDataStructures.h
 //  POSDataStructures
 //
-//  Created by Pavel Osipov on 12.03.14.
-//  Copyright (c) 2013 Pavel Osipov. All rights reserved.
+//  Created by dmitry.golubev on 11.10.2023.
+//  Copyright © 2023 Pavel Osipov. All rights reserved.
 //
 
-#import "POSBinaryHeap.h"
+#import <Foundation/Foundation.h>
+#import <POSDataStructures/POSBinaryHeap.h>
+#import <POSDataStructures/POSDataStructures-Prefix.h>
+
+//! Project version number for POSDataStructures.
+FOUNDATION_EXPORT double POSDataStructuresVersionNumber;
+
+//! Project version string for POSDataStructures.
+FOUNDATION_EXPORT const unsigned char POSDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POSDataStructures/PublicHeader.h>
+
+
